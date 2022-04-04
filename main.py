d87b9911b1de0649fbf904f96b8b1b70f95b2c02
AK7795 <akshaysingodi@gmail.com>
2022-04-04 15:40:44 +0530

    tx

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def welcom():
     return "welcome to my website"
 
+
 @app.route("/contact")
 def cont():
-    return "Contacts displayed"
+    return render_template("contact.html")
+
 
 @app.route("/home")
 def home():
     return "home page "
 
+
 @app.route("/gallery")
 def gall():
-    return "gallery"
+    return "add ur pics here"
 
 
 if __name__ == "__main__":
